39ae672a5669a006bc3f8c3e9c5765e123f72cd5
codecustard <109494546+vimalveeramani@users.noreply.github.com>
2023-07-05 16:17:44 +0530

    Create DataType.java

---
+++ DataType.java
@@ -0,0 +1,13 @@
+public class main{
+  public static void main(string[] args){
+    String var1 = "Spartans never retreat!"; //string datatype
+    int var2 = 19; // integer type
+    double var3 = 4.560; //Double type to store the decimal point value
+    boolean var4 = true; // true or false based on input
+    System.out.println(var1);
+    System.out.println(var2);
+    System.out.println(var3);
+    System.out.println(var4);
+  // These are the basic data type
+  }
+}
