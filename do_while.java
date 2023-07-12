5e15e32f642502965ee6167839dba9c97518a9c4
codecustard <109494546+vimalveeramani@users.noreply.github.com>
2023-07-12 10:29:45 +0530

    Create do_while.java

---
+++ do_while.java
@@ -0,0 +1,10 @@
+public class do_while
+{
+    
+    public static void main(String[] args){
+        int count = 0;
+        do{
+            System.out.println("Welcome");
+        }while( count < 0);
+    }
+}
