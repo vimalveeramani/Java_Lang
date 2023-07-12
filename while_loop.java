eb1710cfcbadb88d6d8f4c646a3916346499baaa
codecustard <109494546+vimalveeramani@users.noreply.github.com>
2023-07-12 10:26:05 +0530

    Create while_loop.java

---
+++ while_loop.java
@@ -0,0 +1,12 @@
+public class loop
+{
+    public static void main(String[] args){
+    int count = 0;
+    while (count < 10) {
+        System.out.println("Welcome to Java!");
+        count = count + 1;
+    }
+        
+        
+    }
+}
